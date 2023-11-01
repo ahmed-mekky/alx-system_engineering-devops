980769fc67b6b07727eeeb1c7fb7db461d6ef3a5
ahmed-mekky <27medsh3pan@gmail.com>
2023-11-01 04:56:52 +0200

    regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:)[^[]+(?<=to:)[^[]+(?=<flags:)[^[]+/).join(",")
+puts ARGV[0].scan(/(?<=from:)[1-9A-Za-z+]+ (?<=to:)[1-9A-Za-z+]+ (?=<flags:)[1-9:+-]+/).join(",")
