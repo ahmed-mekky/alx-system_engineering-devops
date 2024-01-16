6de0c22d82939c56c79afb2c1acfd5f1ff884d7c
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-16 20:13:35 +0200

    lb

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,26 +1,5 @@
 # puppet touching the server
-exec { 'update':
-        command  => '/usr/bin/apt-get update',
-        provider => 'shell'
-}
-
-package { 'nginx':
-  ensure  => 'installed',
-}
-
-file {'/var/www/html/index.html':
-  path    => '/var/www/html/index.html',
-  content => 'Hello World!',
-}
-
-exec {'redirect_me':
-  command  => 'sed -i "23i rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell'
-}
 exec {'add_header':
-  command  => 'sed -i "48i add_header X-Served-By '\$hostname'" /etc/nginx/sites-available/default',
+  command  => 'sed -i "48i add_header X-Served-By "\$hostname"" /etc/nginx/sites-available/default',
   provider => 'shell'
-}
-service {'nginx':
-  ensure  => running,
 }
