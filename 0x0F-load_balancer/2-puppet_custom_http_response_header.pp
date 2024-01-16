d0a45eb131505613de3c276e9e53c9bcbad6c9d3
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-16 21:24:51 +0200

    lb

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,27 +1,29 @@
 # puppet touching the server
-exec { 'update':
-        command  => '/usr/bin/apt-get update',
-        provider => 'shell'
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['update system']
+        ensure => 'installed',
+        require => Exec['update system']
 }
 
 file {'/var/www/html/index.html':
-  content => 'Hello World!',
+        content => 'Hello World!'
 }
 
 exec {'redirect_me':
-  command  => 'sed -i "23i\	 rewrite ^/redirect_me https://www.youtube.com/ permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell'
+        command => 'sed -i "24i\        rewrite ^/redirect_me https://www.youtube.com/ permanent;" /etc/nginx/sites-available/default',
+        provider => 'shell'
 }
+
 exec {'HTTP header':
-  command  => 'sed -i "24i\	 add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-  provider => 'shell'
+        command => 'sed -i "25i\        add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+        provider => 'shell'
 }
+
 service {'nginx':
-  ensure  => running,
-  require => Package['nginx']
+        ensure => running,
+        require => Package['nginx']
 }
