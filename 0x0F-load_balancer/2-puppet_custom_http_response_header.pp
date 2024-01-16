47d1565c134bc00a7e79fc4e85b3c459a7a2fe3f
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-16 21:08:59 +0200

    lb

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,8 @@
 # puppet touching the server
 exec {'add_header':
-  command  => 'sed -i "48i add_header X-Served-By "\$hostname"" /etc/nginx/sites-available/default',
+  command  => 'sed -i "49i add_header X-Served-By "\$hostname";" /etc/nginx/sites-available/default',
   provider => 'shell'
+}
+service {'nginx':
+  ensure  => running,
 }
