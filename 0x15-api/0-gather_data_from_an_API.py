e74f78625ec4b7d11b0dee78ddd2d50cb8139efc
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-30 02:02:29 +0200

    first_api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import requests
+import json
+from sys import argv
+
+if __name__ == "__main__":
+
+    with requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
+    ) as json_response:
+        todos = json.loads(json_response.content)
+        with requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+        ) as json_user:
+            user_dict = json.loads(json_user.content)
+            employee_name = user_dict.get("name")
+        completed_tasks, total_tasks = 0, 0
+        for todo in todos:
+            if todo.get("completed") is True:
+                completed_tasks += 1
+            total_tasks += 1
+        print(
+            f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):"
+        )
+        for todo in todos:
+            print(f"     {todo.get('title')}")
