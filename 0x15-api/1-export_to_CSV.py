3cbb591cb5f2c9bee77b28529ce3710f202733fd
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-30 03:08:43 +0200

    api

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""first api module"""
+import csv
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    with requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
+    ) as json_response:
+        todos = json.loads(json_response.content)
+        with requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+        ) as json_user:
+            user_dict = json.loads(json_user.content)
+            employee_name = user_dict.get("name")
+    for todo in todos:
+        todo["name"] = employee_name
+        todo.pop("id")
+    user_id = todos[0].get("userId")
+    fields = ["userId", "name", "completed", "title"]
+    with open(f"{user_id}.csv", "w", newline="") as csv_file:
+        writer = csv.DictWriter(
+            csv_file, quoting=csv.QUOTE_NONNUMERIC, fieldnames=fields
+        )
+        writer.writerows(todos)
