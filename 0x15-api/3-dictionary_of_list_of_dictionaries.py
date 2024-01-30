9376470e1829276827ec43e2cf08b8e6e7898fc6
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-30 04:43:08 +0200

    last_api

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""first api module"""
+import json
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    user_todo = {}
+    i = 1
+    for user in response:
+        with requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{i}/todos"
+        ) as json_response:
+            todos = json.loads(json_response.content)
+            employee_name = user.get("username")
+        for todo in todos:
+            todo["username"] = employee_name
+            todo["task"] = todo.get("title")
+            todo.pop("id")
+            todo.pop("title")
+            todo.pop("userId")
+        user_todo[f"{i}"] = todos
+        i += 1
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(user_todo, json_file)
