4148af9146484bd250559892f33feab2b4a450b2
ahmed-mekky <27medsh3pan@gmail.com>
2024-02-14 04:59:11 +0200

    idk

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,17 +4,17 @@ def recurse(subreddit, hot_list=[], after=None):
     Get the number of subscribers for a given subreddit.
     """
     if subreddit:
-        if after == None:
+        if after is None:
             url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=1'
         else:
             url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=1' + after
-        res = requests.get(url,headers={'User-Agent': 'A Python script'}, allow_redirects=False)
+        res = requests.get(url, headers={'User-Agent': 'A Python script'}, allow_redirects=False)
         if res.status_code == 200:
             json_res = res.json()
-            hot_list.append(json_res.get('data').get('children').get('data').get('title'))
-            after_post = f'&after={json_res.get('data').get('after')}'
+            hot_list.append(json_res['data']['children'][0]['data']['title'])
+            after_post = f'&after={json_res["data"]["after"]}'
         else:
             return None
-        if json_res.get('data').get('after') == None:
+        if json_res['data']['after'] is None:
             return hot_list
-        recurse(subreddit, hot_list=[], after=after_post)
+        return recurse(subreddit, hot_list, after=after_post)
