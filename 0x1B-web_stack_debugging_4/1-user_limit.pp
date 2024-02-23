2d1680ac2e9221e93a7f0b28442e36c877675e41
ahmed-mekky <27medsh3pan@gmail.com>
2024-02-23 03:55:08 +0200

    web_debug_4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
-# comment
+# giving file limits for user holberton
 
-exec { 'giving limits to holberton':
+exec { 'giving_limits':
   command  => 'sed -i "s/nofile 5/nofile 4096/" /etc/security/limits.conf',
   provider  => shell
 }
-exec { 'more limits':
+exec { 'more_limits':
   command  => 'sed -i "s/nofile 4/nofile 2048/" /etc/security/limits.conf',
   provider  => shell
 }
