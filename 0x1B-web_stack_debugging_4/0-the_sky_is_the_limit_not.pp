72b8187b259c097f93399952b1039ef8afc05b91
ahmed-mekky <27medsh3pan@gmail.com>
2024-02-23 04:02:30 +0200

    web_debug_4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # fixing nginx file limit
 
 exec {'fixing_limits':
-  command     => 'sed -i "s/15/4096/" /etc/default/nginx',
-  provider    => shell
+  command  => 'sed -i "s/15/4096/" /etc/default/nginx',
+  provider => shell
 }
 
 exec {'nginx-restart':
-  command     => 'service nginx restart',
-  provider    => shell
+  command  => 'service nginx restart',
+  provider => shell
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,9 +2,9 @@
 
 exec { 'giving_limits':
   command  => 'sed -i "s/nofile 5/nofile 4096/" /etc/security/limits.conf',
-  provider  => shell
+  provider => shell
 }
 exec { 'more_limits':
   command  => 'sed -i "s/nofile 4/nofile 2048/" /etc/security/limits.conf',
-  provider  => shell
+  provider => shell
 }
